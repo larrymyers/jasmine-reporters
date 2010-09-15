d24f9cf291b0f346b3600fe151e54b12d461df62
Larry Myers <larry@larrymyers.com>
2010-09-15 17:47:57 -0400

    working JUnit reporter, exports junit xml report correctly to file

--- src/jasmine.junit_reporter.js
+++ src/jasmine.junit_reporter.js
@@ -4,6 +4,19 @@
         throw new Exception("jasmine library does not exist in global namespace!");
     }
     
+    function writeFile(filename, text) {
+        if (! java) {
+            return;
+        }
+        
+        try {
+            var out = new java.io.BufferedWriter(new java.io.FileWriter(filename));
+            out.write(text);
+            out.close();
+        } catch (e) {
+        }
+    }
+    
     var JUnitXmlReporter = function() {
         this.output = [];
     };
@@ -32,7 +45,44 @@
         },
         
         reportSuiteResults: function(suite) {
-            var results = suite.results();
+            var output = [],
+                fileName = 'TEST-' + suite.description.replace(/\s/g, '') + '.xml',
+                results = suite.results(),
+                items = results.getItems(),
+                item,
+                expectedResults,
+                trace,
+                i,
+                j;
+            
+            output.push('<?xml version="1.0" encoding="UTF-8" ?>');
+            output.push('<testsuite name="' + suite.description + '" errors="0" failures="' 
+                + results.failedCount + '" tests="' + results.totalCount + '" time="0.064" timestamp="2010-09-15T20:41:40">');
+            
+            for (i = 0; i < items.length; i++) {
+                item = items[i];
+                
+                output.push(' <testcase classname="' + suite.description + '" name="' + item.description + '" time="0.0080">');
+                
+                if (!item.passed()) {
+                    expectedResults = item.getItems();
+                    
+                    for (j = 0; j < expectedResults.length; j++) {
+                        trace = expectedResults[j].trace;
+                        
+                        if (trace instanceof Error) {
+                            output.push('<failure>' + trace.message + '</failure>');
+                            break;
+                        }
+                    }
+                }
+                
+                output.push('</testcase>');
+            }
+            
+            output.push('</testsuite>');
+            
+            writeFile(fileName, output.join(''));
             
             this.log(suite.description + ": " + results.passedCount + " of " + results.totalCount + " passed.");
         },
