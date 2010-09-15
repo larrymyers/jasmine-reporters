3d5b7d59a1085fe5f70e4b13e4cf8f00429a2bff
Larry Myers <larry@larrymyers.com>
2010-09-14 23:54:08 -0400

    console reporter setup

--- src/jasmine.console_reporter.js
+++ src/jasmine.console_reporter.js
@@ -8,7 +8,35 @@
         
     };
     
-    ConsoleReporter.prototype = jasmine.JsApiReporter;
+    ConsoleReporter.prototype = {
+        reportRunnerResults: function(runner) {
+            
+        },
+
+        reportRunnerStarting: function(runner) {
+            
+        },
+
+        reportSpecResults: function(spec) {
+            
+        },
+
+        reportSpecStarting: function(spec) {
+            
+        },
+
+        reportSuiteResults: function(suite) {
+            
+        },
+        
+        log: function(str) {
+            var console = jasmine.getGlobal().console;
+            
+            if (console && console.log) {
+                console.log(str)
+            }
+        }
+    };
     
     
     
