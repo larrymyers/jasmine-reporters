66c93acf3a37317ad93c199326daad3eaf25956a
Ben Loveridge <bloveridge@movenetworks.com>
2011-01-02 18:52:07 -0700

    Issue #1: handle nested describes gracefully.
    Includes a flag (default: true) to indicate whether nested results should be saved into a single XML file (wrapped inside a <testsuites> block).
    
    Much of the logic was moved into different blocks -- SpecResults generates its <testcase> output; SuiteResults generates the <testsuite> output (including its spec's <testcase> blocks); RunnerResults puts them all together and saves them out to the right file(s).

--- src/jasmine.junit_reporter.js
+++ src/jasmine.junit_reporter.js
@@ -1,25 +1,17 @@
 (function() {
-    
+
     if (! jasmine) {
         throw new Exception("jasmine library does not exist in global namespace!");
     }
-    
-    function writeFile(filename, text) {
-       try {
-            var out = new java.io.BufferedWriter(new java.io.FileWriter(filename));
-            out.write(text);
-            out.close();
-        } catch (e) {
-        }
-    }
-    
+
+
     function elapsed(startTime, endTime) {
         return (endTime - startTime)/1000;
     }
-    
+
     function ISODateString(d) {
         function pad(n) { return n < 10 ? '0'+n : n; }
-        
+
         return d.getFullYear() + '-'
             + pad(d.getMonth()+1) +'-'
             + pad(d.getDate()) + 'T'
@@ -27,105 +19,145 @@
             + pad(d.getMinutes()) + ':'
             + pad(d.getSeconds());
     }
-    
+
     /**
      * Generates JUnit XML for the given spec run.
      * Allows the test results to be used in java based CI
      * systems like CruiseControl and Hudson.
+     *
+     * @param {string} savePath where to save the files
+     * @param {boolean} consolidate whether to save nested describes within the
+     *                  same file as their parent; default: true
      */
-    var JUnitXmlReporter = function(savePath) {
+    var JUnitXmlReporter = function(savePath, consolidate) {
         this.savePath = savePath || '';
+        this.consolidate = consolidate === jasmine.undefined ? true : consolidate;
     };
-    
+
     JUnitXmlReporter.prototype = {
-        reportRunnerResults: function(runner) {
-            this.log("Runner Finished.");
-        },
-        
         reportRunnerStarting: function(runner) {
             this.log("Runner Started.");
         },
-        
-        reportSpecResults: function(spec) {
-            var resultText = "Failed.";
-            
-            if (spec.results().passed()) {
-                resultText = "Passed.";
-            }
-            
-            spec.endTime = new Date();
-            
-            this.log(resultText);
-        },
-        
+
         reportSpecStarting: function(spec) {
             spec.startTime = new Date();
-            
+
             if (! spec.suite.startTime) {
-                spec.suite.startTime = new Date();
+                spec.suite.startTime = spec.startTime;
             }
-            
+
             this.log(spec.suite.description + ' : ' + spec.description + ' ... ');
         },
-        
-        reportSuiteResults: function(suite) {
-            var output = [],
-                fileName = 'TEST-' + suite.description.replace(/\s/g, '') + '.xml',
-                results = suite.results(),
-                items = results.getItems(),
-                item,
-                spec,
-                expectedResults,
-                trace,
-                i,
-                j;
-                
-            suite.endTime = new Date();
-            
-            output.push('<?xml version="1.0" encoding="UTF-8" ?>');
-            output.push('<testsuite name="' + suite.description + '" errors="0" failures="' 
-                + results.failedCount + '" tests="' + results.totalCount + '" time="' 
-                + elapsed(suite.startTime, suite.endTime) + '" timestamp="' + ISODateString(suite.startTime) + '">');
-            
-            for (i = 0; i < items.length; i++) {
-                item = items[i];
-                spec = suite.specs()[i];
-                
-                output.push(' <testcase classname="' + suite.description + '" name="' 
-                    + item.description + '" time="' + elapsed(spec.startTime, spec.endTime) + '">');
-                
-                if (!item.passed()) {
-                    expectedResults = item.getItems();
-                    
-                    for (j = 0; j < expectedResults.length; j++) {
-                        trace = expectedResults[j].trace;
-                        
-                        if (trace instanceof Error) {
-                            output.push('<failure>' + trace.message + '</failure>');
-                            break;
-                        }
-                    }
-                }
-                
-                output.push('</testcase>');
+
+        reportSpecResults: function(spec) {
+            var results = spec.results();
+            spec.didFail = !results.passed();
+            spec.status = spec.didFail ? 'Failed.' : 'Passed.';
+            if (results.skipped) {
+                spec.status = 'Skipped.';
             }
-            
-            output.push('</testsuite>');
-            
-            writeFile(this.savePath + fileName, output.join(''));
-            
-            this.log(suite.description + ": " + results.passedCount + " of " + results.totalCount + " passed.");
+            this.log(spec.status);
+
+            spec.duration = elapsed(new Date(), spec.startTime);
+            spec.output = '<testcase classname="' + spec.suite.getFullName() +
+                '" name="' + spec.description + '" time="' + spec.duration + '">';
+
+            var failure = "";
+            var failures = 0;
+            var resultItems = results.getItems();
+            for (var i = 0; i < resultItems.length; i++) {
+                var result = resultItems[i];
+
+                if (result.type == 'expect' && result.passed && !result.passed()) {
+                    failures += 1;
+                    failure += (failures + ": " + result.message + " ");
+                }
+            }
+            if (failure) {
+                spec.output += "<failure>" + failure.trim() + "</failure>";
+            }
+            spec.output += "</testcase>";
         },
-        
+
+        reportSuiteResults: function(suite) {
+            var results = suite.results();
+            var specs = suite.specs();
+            var specOutput = "";
+            // for JUnit results, let's only include directly failed tests (not nested suites')
+            var failedCount = 0;
+
+            suite.status = results.passed() ? 'Passed.' : 'Failed.';
+            if (results.totalCount === 0) { // todo: change this to check results.skipped
+                suite.status = 'Skipped.';
+            }
+
+            // if a suite has no (active?) specs, reportSpecStarting is never called
+            // and thus the suite has no startTime -- account for that here
+            suite.startTime = suite.startTime || new Date();
+            suite.duration = elapsed(new Date(), suite.startTime);
+
+            for (var i = 0; i < specs.length; i++) {
+                failedCount += specs[i].didFail ? 1 : 0;
+                specOutput += "\n  " + specs[i].output;
+            }
+            suite.output = '\n<testsuite name="' + suite.getFullName() +
+                '" errors="0" tests="' + specs.length + '" failures="' + failedCount +
+                '" time="' + suite.duration + '" timestamp="' + ISODateString(suite.startTime) + '">';
+            suite.output += specOutput;
+            suite.output += "\n</testsuite>";
+            this.log(suite.description + ": " + results.passedCount + " of " + results.totalCount + " expectations passed.");
+        },
+
+        reportRunnerResults: function(runner) {
+            this.log("Runner Finished.");
+            var suites = runner.suites();
+            for (var i = 0; i < suites.length; i++) {
+                var suite = suites[i];
+                var fileName = 'TEST-' + suite.getFullName().replace(/\s/g, '') + '.xml';
+                var output = '<?xml version="1.0" encoding="UTF-8" ?>';
+                // if we are consolidating, only write out top-level suites
+                if (this.consolidate && suite.parentSuite) {
+                    continue;
+                }
+                else if (this.consolidate) {
+                    output += "\n<testsuites>";
+                    output += this.getNestedOutput(suite);
+                    output += "\n</testsuites>";
+                    this.writeFile(this.savePath + fileName, output);
+                }
+                else {
+                    output += suite.output;
+                    this.writeFile(this.savePath + fileName, output);
+                }
+            }
+        },
+
+        getNestedOutput: function(suite) {
+            var output = suite.output;
+            for (var i = 0; i < suite.suites().length; i++) {
+                output += this.getNestedOutput(suite.suites()[i]);
+            }
+            return output;
+        },
+
+        writeFile: function(filename, text) {
+           try {
+                var out = new java.io.BufferedWriter(new java.io.FileWriter(filename));
+                out.write(text);
+                out.close();
+            } catch (e) {
+            }
+        },
+
         log: function(str) {
             var console = jasmine.getGlobal().console;
-            
+
             if (console && console.log) {
                 console.log(str);
             }
         }
     };
-    
+
     // export public
     jasmine.JUnitXmlReporter = JUnitXmlReporter;
 })();
