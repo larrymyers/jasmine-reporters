e7ed15d7bf853bf98ba392bf78c5fd872c5d8038
Ben Loveridge <benl@gradecam.com>
2014-04-18 12:15:32 -0600

    Expose reporters in Node in a more friendly fashion

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports.JUnitXmlReporter = require("./src/jasmine.junit_reporter").JUnitXmlReporter;
+//module.exports.NUnitXmlReporter = require("./src/jasmine.nunit_reporter").NUnitXmlReporter;
+//module.exports.TeamcityReporter = require("./src/jasmine.teamcity_reporter.js").TeamcityReporter;
+//module.exports.TapReporter = require("./src/jasmine.tap_reporter.js").TapReporter;
+//module.exports.TerminalReporter = require("./src/jasmine.tap_reporter.js").TerminalReporter;
--- src/load_reporters.js
+++
@@ -1,4 +0,0 @@
-require("./jasmine.junit_reporter.js")
-require("./jasmine.nunit_reporter.js")
-require("./jasmine.teamcity_reporter.js")
-require("./jasmine.tap_reporter.js")
