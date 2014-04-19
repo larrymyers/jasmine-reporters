7c499c476bfaf29bd140dc3e92a53353dcc473b9
Ben Loveridge <benl@gradecam.com>
2014-04-18 22:39:41 -0600

    expose TerminalReporter in node.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports.JUnitXmlReporter = require("./src/jasmine.junit_reporter").JUnitXmlReporter;
 module.exports.NUnitXmlReporter = require("./src/jasmine.nunit_reporter").NUnitXmlReporter;
+module.exports.TerminalReporter = require("./src/jasmine.terminal_reporter.js").TerminalReporter;
 //module.exports.TeamcityReporter = require("./src/jasmine.teamcity_reporter.js").TeamcityReporter;
 //module.exports.TapReporter = require("./src/jasmine.tap_reporter.js").TapReporter;
-//module.exports.TerminalReporter = require("./src/jasmine.tap_reporter.js").TerminalReporter;
