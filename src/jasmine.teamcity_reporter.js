e6337b6fb5731b48182547d741919915f4d439c7
Ben Loveridge <benl@gradecam.com>
2014-04-21 10:48:16 -0600

    Update TeamCity reporter for Jasmine2 (and renamed from TeamcityReporter -> TeamCityReporter)

--- src/jasmine.teamcity_reporter.js
+++ src/jasmine.teamcity_reporter.js
@@ -1,143 +1,144 @@
-(function() {
-    if (! jasmine) {
-        throw new Exception("jasmine library does not exist in global namespace!");
+(function(global) {
+    var UNDEFINED,
+        exportObject;
+
+    if (typeof module !== "undefined" && module.exports) {
+        exportObject = exports;
+    } else {
+        exportObject = global.jasmineReporters = global.jasmineReporters || {};
+    }
+
+    function isFailed(obj) { return obj.status === "failed"; }
+    function isSkipped(obj) { return obj.status === "pending"; }
+    function pad(n) { return n < 10 ? '0'+n : n; }
+    function padThree(n) { return n < 10 ? '00'+n : n < 100 ? '0'+n : n; }
+    function ISODateString(d) {
+        return d.getUTCFullYear() + '-' +
+            pad(d.getUTCMonth()+1) + '-' +
+            pad(d.getUTCDate()) + 'T' +
+            pad(d.getUTCHours()) + ':' +
+            pad(d.getUTCMinutes()) + ':' +
+            pad(d.getUTCSeconds()) + '.' +
+            // TeamCity wants ss.SSS
+            padThree(d.getUTCMilliseconds()) +
+            'Z';
     }
 
     /**
-     * Basic reporter that outputs spec results to for the Teamcity build system
+     * Basic reporter that outputs spec results for the TeamCity build system
      *
      * Usage:
      *
-     * jasmine.getEnv().addReporter(new jasmine.TeamcityReporter());
-     * jasmine.getEnv().execute();
+     * jasmine.addReporter(new jasmineReporters.TeamCityReporter());
      */
-    var TeamcityReporter = function() {
-        this.started = false;
-        this.finished = false;
+    exportObject.TeamCityReporter = function() {
+        var self = this;
+        self.started = false;
+        self.finished = false;
+
+        var currentSuite = null,
+            totalSpecsExecuted = 0,
+            totalSpecsSkipped = 0,
+            totalSpecsFailed = 0,
+            totalSpecsDefined;
+
+        self.jasmineStarted = function(summary) {
+            totalSpecsDefined = summary && summary.totalSpecsDefined || NaN;
+            exportObject.startTime = new Date();
+            self.started = true;
+            tclog("progressStart 'Running Jasmine Tests'");
+        };
+        self.suiteStarted = function(suite) {
+            suite._parent = currentSuite;
+            currentSuite = suite;
+            tclog("testSuiteStarted", {
+                name: suite.description
+            });
+        };
+        self.specStarted = function(spec) {
+            tclog("testStarted", {
+                name: spec.description,
+                captureStandardOutput: 'true'
+            });
+        };
+        self.specDone = function(spec) {
+            if (isSkipped(spec)) {
+                tclog("testIgnored", {
+                    name: spec.description
+                });
+            }
+            // TeamCity specifies there should only be a single `testFailed`
+            // message, so we'll only grab the first failedExpectation
+            if (isFailed(spec) && spec.failedExpectations.length) {
+                var failure = spec.failedExpectations[0];
+                tclog("testFailed", {
+                    name: spec.description,
+                    message: failure.message,
+                    details: failure.stack
+                });
+            }
+            tclog("testFinished", {
+                name: spec.description
+            });
+        };
+        self.suiteDone = function(suite) {
+            // disabled suite (xdescribe) -- suiteStarted was never called
+            if (suite._parent === UNDEFINED) {
+                self.suiteStarted(suite);
+                suite._disabled = true;
+            }
+            tclog("testSuiteFinished", {
+                name: suite.description
+            });
+            currentSuite = suite._parent;
+        };
+        self.jasmineDone = function() {
+            tclog("progressFinish 'Running Jasmine Tests'");
+
+            self.finished = true;
+            // this is so phantomjs-testrunner.js can tell if we're done executing
+            exportObject.endTime = new Date();
+        };
     };
 
-    TeamcityReporter.prototype = {
-        reportRunnerResults: function(runner) {
-            this.log("##teamcity[progressFinish 'Running Jasmine Tests']");
-        },
-
-        reportRunnerStarting: function(runner) {
-            this.log("##teamcity[progressStart 'Running Jasmine Tests']");
-        },
-
-        reportSpecResults: function(spec) { },
-
-        reportSpecStarting: function(spec) { },
-
-        reportSuiteResults: function(suite) {
-            var results = suite.results();
-            var path = [];
-            while(suite) {
-                path.unshift(suite.description);
-                suite = suite.parentSuite;
+    function log(str) {
+        var console = global.console;
+        if (console && console.log) {
+            console.log(str);
+        }
+    }
+    // shorthand for logging TeamCity messages
+    function tclog(message, attrs) {
+        var str = "##teamcity[" + message;
+        if (typeof(attrs) === "object") {
+            if (!("timestamp" in attrs)) {
+                attrs.timestamp = new Date();
             }
-            var description = path.join(' ');
-
-            this.log("##teamcity[testSuiteStarted name='" + this.escapeTeamcityString(description) + "']");
-
-            var outerThis = this;
-            var eachSpecFn = function(spec){
-                if (spec.description) {
-                    outerThis.log("##teamcity[testStarted name='" + outerThis.escapeTeamcityString(spec.description) + "' captureStandardOutput='true']");
-                    var specResultFn = function(result){
-                        if (!result.passed_) {
-                            outerThis.log("##teamcity[testFailed name='" + outerThis.escapeTeamcityString(spec.description) + "' message='|[FAILED|]' details='" + outerThis.escapeTeamcityString(result.trace.stack) + "']");
-                        }
-                    };
-
-                    for (var j = 0, jlen = spec.items_.length; j < jlen; j++) {
-                        specResultFn(spec.items_[j]);
-                    }
-                    outerThis.log("##teamcity[testFinished name='" + outerThis.escapeTeamcityString(spec.description) + "']");
+            for (var prop in attrs) {
+                if (attrs.hasOwnProperty(prop)) {
+                    str += " " + prop + "='" + escapeTeamCityString(attrs[prop]) + "'";
                 }
-            };
-            for (var i = 0, ilen = results.items_.length; i < ilen; i++) {
-                eachSpecFn(results.items_[i]);
-            }
-
-
-
-            this.log("##teamcity[testSuiteFinished name='" + outerThis.escapeTeamcityString(description) + "']");
-        },
-
-        log: function(str) {
-            var console = jasmine.getGlobal().console;
-            if (console && console.log) {
-                console.log(str);
-            }
-        },
-
-        hasGroupedConsole: function() {
-            var console = jasmine.getGlobal().console;
-            return console && console.info && console.warn && console.group && console.groupEnd && console.groupCollapsed;
-        },
-
-        escapeTeamcityString: function(message) {
-            if(!message) {
-                return "";
-            }
-
-            return message.replace(/\|/g, "||")
-                          .replace(/\'/g, "|'")
-                          .replace(/\n/g, "|n")
-                          .replace(/\r/g, "|r")
-                          .replace(/\u0085/g, "|x")
-                          .replace(/\u2028/g, "|l")
-                          .replace(/\u2029/g, "|p")
-                          .replace(/\[/g, "|[")
-                          .replace(/]/g, "|]");
-        }
-    };
-
-    function suiteResults(suite) {
-        console.group(suite.description);
-        var specs = suite.specs();
-        for (var i in specs) {
-            if (specs.hasOwnProperty(i)) {
-                specResults(specs[i]);
             }
         }
-        var suites = suite.suites();
-        for (var j in suites) {
-            if (suites.hasOwnProperty(j)) {
-                suiteResults(suites[j]);
-            }
-        }
-        console.groupEnd();
+        str += "]";
+        log(str);
     }
+    function escapeTeamCityString(str) {
+        if(!str) {
+            return "";
+        }
+        if (Object.prototype.toString.call(str) === '[object Date]') {
+            return ISODateString(str);
+        }
 
-    function specResults(spec) {
-        var results = spec.results();
-        if (results.passed() && console.groupCollapsed) {
-            console.groupCollapsed(spec.description);
-        } else {
-            console.group(spec.description);
-        }
-        var items = results.getItems();
-        for (var k in items) {
-            if (items.hasOwnProperty(k)) {
-                itemResults(items[k]);
-            }
-        }
-        console.groupEnd();
+        return str.replace(/\|/g, "||")
+            .replace(/\'/g, "|'")
+            .replace(/\n/g, "|n")
+            .replace(/\r/g, "|r")
+            .replace(/\u0085/g, "|x")
+            .replace(/\u2028/g, "|l")
+            .replace(/\u2029/g, "|p")
+            .replace(/\[/g, "|[")
+            .replace(/]/g, "|]");
     }
-
-    function itemResults(item) {
-        if (item.passed && !item.passed()) {
-            console.warn({actual:item.actual,expected: item.expected});
-            item.trace.message = item.matcherName;
-            console.error(item.trace);
-        } else {
-            console.info('Passed');
-        }
-    }
-
-    // export public
-    jasmine.TeamcityReporter = TeamcityReporter;
-})();
-
+})(this);
