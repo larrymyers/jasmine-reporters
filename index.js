2930af94cf81a70f87dec0518bfa1b83ff2edce0
Ben Loveridge <benl@gradecam.com>
2014-07-02 15:23:00 -0600

    Rename reporters from `jasmine.REPORTERNAME.js” to just “REPORTERNAME.js”

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports.JUnitXmlReporter = require("./src/jasmine.junit_reporter").JUnitXmlReporter;
-module.exports.NUnitXmlReporter = require("./src/jasmine.nunit_reporter").NUnitXmlReporter;
-module.exports.TapReporter = require("./src/jasmine.tap_reporter.js").TapReporter;
-module.exports.TeamCityReporter = require("./src/jasmine.teamcity_reporter.js").TeamCityReporter;
-module.exports.TerminalReporter = require("./src/jasmine.terminal_reporter.js").TerminalReporter;
+module.exports.JUnitXmlReporter = require("./src/junit_reporter").JUnitXmlReporter;
+module.exports.NUnitXmlReporter = require("./src/nunit_reporter").NUnitXmlReporter;
+module.exports.TapReporter = require("./src/tap_reporter.js").TapReporter;
+module.exports.TeamCityReporter = require("./src/teamcity_reporter.js").TeamCityReporter;
+module.exports.TerminalReporter = require("./src/terminal_reporter.js").TerminalReporter;
